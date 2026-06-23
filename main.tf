e556cc340e0294733fa3306ea4e2a52cb15e1ba2
DanielObirije <obirijedaniel@gmail.com>
2026-06-23 23:37:55 +0100

    added dynamoDb and lambda permissions

--- main.tf
+++ main.tf
@@ -50,5 +50,49 @@ resource "aws_dynamodb_table" "url_storage" {
   )
 }
 
-//lambda
+//iam role and permissions for lambda
+resource "aws_iam_role" "lambda_execution" {
+  name = "${local.name_prefix}-lambda-role-${local.name_surfix}"
+  assume_role_policy = jsonencode({
+  Version = "2012-10-17"
+  Statement = {
+     Action = "sts:AssumeRole"
+     Effect = "Allow"
+     Principal = {
+        Service = "lambda.amazonaws.com"
+     }
+  }
+})
+tags = local.common_tags
+}
 
+resource "aws_iam_role_policy_attachment" "lambda_execution_policy_attachement" {
+  role = aws_iam_role.lambda_execution.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_policy" "dynamodb_access" {
+   name = "${local.name_prefix}-dynamodb-plicy-${local.name_surfix}"
+   description = "IAM policy for DynamoDB access from lambda "
+   policy = jsondecode({
+      Version = "2012-10-17"
+      Statement = {
+        Effect = "Allow"
+        Action = [
+          "dynamodb:GetItem",
+          "dynamodb:PutItem",
+          "dynamodb:UpdateItem",
+          "dynamodb:DeleteItem",
+          "dynamodb:Query",
+          "dynamodb:Scan"
+        ]
+        Resource = aws_dynamodb_table.url_storage.arn
+      }
+   })
+   tags = local.common_tags
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_dynamodb_access" {
+  role = aws_iam_role.lambda_execution.name
+  policy_arn = aws_iam_policy.dynamodb_access.arn
+}
